1d06941f9adb9f3db822a3ae672c9ca5bd3cd470
Nikhel Krishna <krishna.nikhel@paideiaschool.org>
2021-03-05 21:47:21 -0500

    changed to vwlocity control in auton

--- src/Impl_src/match_auton.cpp
+++ src/Impl_src/match_auton.cpp
@@ -18,10 +18,16 @@ void redAuton() {
     Flywheel.spin(fwd,12,volt);
     Indexer.spin(fwd,10,volt);
     task::sleep(4000);
-;
-    chassis.setDrive(-6, -6, -6 , -6 );
+
+    chassis.leftFront.spin(reverse,100,rpm);
+    chassis.leftBack.spin(reverse,100,rpm);
+    chassis.rightBack.spin(reverse,100,rpm);
+    chassis.rightFront.spin(reverse,100,rpm);
     task::sleep(3000);
-    chassis.setDrive(6, 6, 6 , 6 );
+    chassis.leftFront.spin(fwd,100,rpm);
+    chassis.leftBack.spin(fwd,100,rpm);
+    chassis.rightBack.spin(fwd,100,rpm);
+    chassis.rightFront.spin(fwd,100,rpm);
     task::sleep(2000);
     chassis.setDrive(0, 0, 0, 0);
   }
